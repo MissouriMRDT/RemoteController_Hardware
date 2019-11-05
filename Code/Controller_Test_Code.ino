b91bce1b3b2ac1bf6a1152273e3ea7182011b564
labbloe <jla7ym@mst.edu>
2019-11-04 21:14:11 -0600

    Added Test Code
    Also reorganized some files related to PDR

---
+++ Code/Controller_Test_Code.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <MCP3008.h>
+#include <SPI.h>
+#include <LiquidCrystal_I2C.h>
+
+MCP3008 adc(D5, D7, D6, D8);
+int lcdColumns = 16;
+int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x20,2,1,0,4,5,6,7,3,POSITIVE);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("HERE");
+  lcd.begin(16,2);
+  pinMode(D0,OUTPUT);
+  pinMode(D3,INPUT);
+  lcd.setCursor(0,0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("--------------------------");
+  Serial.println("Y-Value:" + String(adc.readADC(0)));
+  Serial.println("X-Value:" + String(adc.readADC(1)));
+  Serial.println("Select:" + String(adc.readADC(5)));
+  lcd.setCursor(0,0);
+  lcd.print("Y-Value: " + String(adc.readADC(0)));
+  lcd.setCursor(0,1);
+  lcd.print("X-Value: " + String(adc.readADC(1)));
+  delay(100);
+  if((adc.readADC(5)) < 100)
+    digitalWrite(D0,HIGH);
+
+  else
+    digitalWrite(D0,LOW);
+
+  Serial.println("Value: " + String(digitalRead(D3)));
+}
