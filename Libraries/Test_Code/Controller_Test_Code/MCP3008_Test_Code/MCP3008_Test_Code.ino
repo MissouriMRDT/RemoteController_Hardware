59a096ff721bdbdec5ae13417e7946a8cb17ef5a
labbloe <54924769+labbloe@users.noreply.github.com>
2020-01-25 14:42:07 -0600

    Test Code Updated

--- Libraries/Test_Code/Controller_Test_Code/MCP3008_Test_Code/MCP3008_Test_Code.ino
+++ Libraries/Test_Code/Controller_Test_Code/MCP3008_Test_Code/MCP3008_Test_Code.ino
@@ -3,7 +3,7 @@
 #include <SPI.h>
 #include <LiquidCrystal_I2C.h>
 
-MCP3008 adc(D5, D7, D6, D8);
+MCP3008 adc(D8, D6, D7, D5);
 int lcdColumns = 16;
 int lcdRows = 2;
 int SD3 = 10;
@@ -20,12 +20,6 @@ void setup() {
   pinMode(D4,INPUT);
   pinMode(SD2,INPUT);
   pinMode(SD3,INPUT);
-  lcd.setCursor(0,0);
-  lcd.print("   Mars Rover   ");
-  lcd.setCursor(0,1);
-  lcd.print("  Design  Team  ");
-  delay(4000);
-  lcd.clear();
 }
 
 void loop() {
@@ -39,23 +33,4 @@ void loop() {
      Serial.println("Select3:" + String(adc.readADC(4)));
       Serial.println("Select4:" + String(adc.readADC(6)));
        Serial.println("Select5:" + String(adc.readADC(7)));
-  lcd.setCursor(0,0);
-  lcd.print("Y-Value: " + String(adc.readADC(0))+ "   ");
-  lcd.setCursor(0,1);
-  lcd.print("X-Value: " + String(adc.readADC(1))+ "   ");
-  delay(500);
-  if((digitalRead(D3) == LOW) || (digitalRead(D4) == LOW) || (digitalRead(9) == LOW) || (digitalRead(10) == LOW))
-    digitalWrite(D0,HIGH);
-
-  else
-    digitalWrite(D0,LOW);
-    
-  Serial.println("Button:" + String(adc.readADC(6)));
-  if(digitalRead(D3) == LOW)
-    {
-      lcd.clear();
-      lcd.print("D3 Button On");
-      delay(2000);
-    }
-    
 }
